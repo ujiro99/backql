2b8e16159e4f1100f7aec2c36e8122df3715e390
Olli Wang <olliwang@ollix.com>
2011-04-26 17:36:25 +0800

    Initial commit.

---
+++ backql.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+# Syntax: backup.sh <mysql|pgsql> (<ssh|ftp> <user[:password]@host>|dropbox [configfile]) [remotedir]
+set -e
+TAG="$HOSTNAME_$(TZ=UTC date +%Y-%m-%d_%H:%M:%S_UTC)"
+FILE="/tmp/$TAG.sql.gz"
+
+[ "$3" ] || {
+	echo "Please specfiy database type, transfer method and target."
+	exit
+}
+
+
+case "$1" in
+	mysql)
+		mysqldump --all-databases | gzip > "$FILE"
+		;;
+	pgsql)
+		pg_dump | gzip > "$FILE"
+		;;
+	*)
+		echo "Sorry, database type $1 is not supported."
+		exit
+		;;
+esac
+
+case "$2" in
+	ssh)
+		scp -q -o BatchMode=yes "$FILE" "$3:$4"
+		;;
+	ftp)
+		curl -sST "$FILE" "ftp://$3/$4"
+		;;
+	dropbox)
+		dropper "$3" "$4" "$FILE"
+		;;
+	*)
+		echo "Sorry, transfer method $2 is not supported."
+		exit
+esac
+
+SIZE="$(stat --printf %s "$FILE")"
+echo "Backup $TAG completed. Its (compressed) size is $SIZE bytes."
+rm -f "$FILE"
