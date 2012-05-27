11723a5abaaad02ffe6b8d2a0494c1e2c9c7d581
ujiro99 <siro.cola@gmail.com>
2012-05-27 23:21:37 +0900

    Add data format

--- backql.sh
+++ backql.sh
@@ -5,37 +5,42 @@ TAG="$HOSTNAME_$(TZ=UTC date +%Y-%m-%d_%H:%M:%S_UTC)"
 FILE="/tmp/$TAG.sql.gz"
 
 [ "$3" ] || {
-	echo "Please specfiy database type, transfer method and target."
-	exit
+    echo "Please specfiy database type, transfer method and target."
+    exit
 }
 
 
 case "$1" in
-	mysql)
-		mysqldump --all-databases | gzip > "$FILE"
-		;;
-	pgsql)
-		pg_dump | gzip > "$FILE"
-		;;
-	*)
-		echo "Sorry, database type $1 is not supported."
-		exit
-		;;
+    mysql)
+        mysqldump --all-databases | gzip > "$FILE"
+        ;;
+    pgsql)
+        pg_dump | gzip > "$FILE"
+        ;;
+    data)
+        FILENAME="$TAG.data.tar.gz"
+        FILE="/tmp/$FILENAME"
+        tar -C "$HOME" -czf "$FILE" "code/."
+        ;;
+    *)
+        echo "Sorry, database type $1 is not supported."
+        exit
+        ;;
 esac
 
 case "$2" in
-	ssh)
-		scp -q -o BatchMode=yes "$FILE" "$3:$4"
-		;;
-	ftp)
-		curl -sST "$FILE" "ftp://$3/$4"
-		;;
-	dropbox)
-		dropper "$3" "$4" "$FILE"
-		;;
-	*)
-		echo "Sorry, transfer method $2 is not supported."
-		exit
+    ssh)
+        scp -q -o BatchMode=yes "$FILE" "$3:$4"
+        ;;
+    ftp)
+        curl -sST "$FILE" "ftp://$3/$4"
+        ;;
+    dropbox)
+        dropper "$3" "$4" "$FILE"
+        ;;
+    *)
+        echo "Sorry, transfer method $2 is not supported."
+        exit
 esac
 
 SIZE="$(stat --printf %s "$FILE")"
